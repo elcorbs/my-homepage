d10c3bbf1b1b6d3ddecad2fd5ce7045fffe6e0b4
Emma <emma@madetech.com>
2022-10-05 16:11:39 +0100

    Add links to my profile

--- ui/src/Pages/Profile.jsx
+++ ui/src/Pages/Profile.jsx
@@ -5,6 +5,11 @@ export default function Profile(){
   return (
     <PageLayout path={["about me"]} >
       Hi, I'm Emma. More about me here...
+
+
+      <a href="https://dandy-apricot-218.notion.site/961cb613e0b24ea287e145442cb79c35?v=4f6a4a05e95e41138787644b763d090b">A colleciton of my learning notes on Notion</a>
+      <a href="https://www.linkedin.com/in/emma-l-corbett/">My LinkedIn profile</a>
+      <a href="https://github.com/elcorbs">My Github profile</a>
     </PageLayout>
   )
 }
